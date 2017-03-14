4ed60f96aa13ffa9cef8819d24e04052de455b3c
King Crawford <crawboy@me.com>
2017-03-14 12:23:24 -0400

    imformat returns finish line boundary vectors

--- imformat.m
+++ imformat.m
@@ -1,4 +1,4 @@
-function image = imformat(str, arrsize)
+function [image, finish_bound1, finish_bound2] = imformat(str, arrsize)
 % Read the file whose name is given in 'str', reduce its dimensions to
 % 'arrsize', and return the result.
 I = double(imread(str));
@@ -7,6 +7,9 @@ GR = imquantize(I, multithresh(I, 1));
 
 GR(GR(:, end) == 2, end) = 1.5;
 GR(end, GR(end, :) == 2) = 1.5;
+
+finish_bound1 = [find(GR(:, end) == 1.5, 1, 'first'); arrsize(2)];
+finish_bound2 = [find(GR(:, end) == 1.5, 1, 'last'); arrsize(2)];
 
 image = GR;
 end
